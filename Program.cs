c5796da0efd9ddbb415a1cfed269a820652fff5f
fausio luis <luisfausio@gmail.com>
2020-12-19 11:47:29 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace I___O_PUT_inNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
